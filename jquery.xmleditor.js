11963d6d9b9fd2b7ea74989a5818f8fba6747ca3
Steven Spungin <steven@spungin.tv>
2014-08-31 14:11:48 -0400

    Mark document dirty after keypress instead of waiting for onChange event.

--- jquery.xmleditor.js
+++ jquery.xmleditor.js
@@ -17,7 +17,9 @@
 	See the License for the specific language governing permissions and
 	limitations under the License.
 
-	Steven Spungin<steven@spungin.tv> - Changed case on sidebar items to match other items in list.
+	Steven Spungin<steven@spungin.tv>
+		Changed case on sidebar items to match other items in list.
+		Mark document dirty after keypress instead of waiting for onChange event.  On change is only called after blur or single-line widget has enter pressed.
  */
 /*
  * jQuery xml Editor
@@ -1792,7 +1794,7 @@ GUIEditor.prototype._initEventBindings = function() {
 		attribute.xmlElement.updated({action : 'attributeRemoved', target : attribute});
 		self.editor.xmlState.documentChangedEvent();
 		event.stopPropagation();
-	}).on('change', '.' + attributeContainerClass + ' > input,.' + attributeContainerClass + ' > textarea,'
+	}).on('change keypress', '.' + attributeContainerClass + ' > input,.' + attributeContainerClass + ' > textarea,'
 			+ '.' + attributeContainerClass + ' > select', function(event){
 		var attribute = $(this).parents('.' + attributeContainerClass).eq(0).data('xmlAttribute');
 		attribute.syncValue();
@@ -1829,7 +1831,7 @@ GUIEditor.prototype._initEventBindings = function() {
 			contentBlock.slideUp(200);
 		}
 		event.stopPropagation();
-	}).on('change', '.element_text', function(event){
+	}).on('change keypress', '.element_text', function(event){
 		var $this = $(this);
 		var xmlElement = $this.parents('.' + xmlElementClass).eq(0).data('xmlObject');
 		var textObject = $this.parents(".xml_node").first().data('xmlObject');
