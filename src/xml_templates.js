d6d9b21a2ab2ec73881322fe2b8de815e81660c2
lfarrell <lfarrell@email.unc.edu>
2015-07-13 08:43:28 -0400

    Tab & click to focus work correctly.

--- src/xml_templates.js
+++ src/xml_templates.js
@@ -25,8 +25,10 @@ XMLTemplates.prototype.createDialog = function() {
         width: 500,
         modal: true,
         buttons: {
-            "Select Template": function() { self.processForm($(this), self); },
-            Cancel: function() {
+            Choose : function() {
+                self.processForm($(this), self);
+            },
+            Cancel : function() {
                 $(this).dialog("close");
 
                 var default_template = self.editor.options.cancelTemplate;
@@ -37,10 +39,6 @@ XMLTemplates.prototype.createDialog = function() {
                    self.editor.loadSchema(self.editor.options.schema);
                 }
             }
-        },
-        close: function() {
-            form[0].reset();
-            $([]).add(self.selected).removeClass("ui-state-error");
         }
     });
 
@@ -54,20 +52,22 @@ XMLTemplates.prototype.createDialog = function() {
  * Don't think we can assume user will build this form themselves
  */
 XMLTemplates.prototype.templateForm = function() {
-    var form = '<div id="dialog-form" title="Please Select a Template">' +
-      '<p class="validateTips">Form field is required.</p>' +
-      '<form>' +
-        '<fieldset>';
+    var form = '<div id="dialog-form" title="Choose Template">' +
+        '<ul>';
+
     for(var i=0; i<this.templates.length; i++) {
-        form += '<input class="templating" name="templating" type="radio" value="' + this.templates[i] + '">' + this.templates[i] + '<br />';
+        form += '<li class="templating"'  + ' id="template_' + i + '">' +
+            '<a href="#">' +
+            '   <i class="fa fa-file-o"></i> ' + this._formatFormText(this.templates[i]) +
+            '</a>'
+            '</li>';
     }
 
-    form += '<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">' +
-       '</fieldset>' +
-      '</form>' +
-    '</div>';
+    form += '</ul>' +
+        '</div>';
 
     $(form).insertAfter("body");
+    $('#dialog-form li:first').addClass('focus');
 };
 
 /**
@@ -77,21 +77,10 @@ XMLTemplates.prototype.templateForm = function() {
  * @returns {boolean}
  */
 XMLTemplates.prototype.processForm = function(dialog, self) {
-    var valid = true;
-    var selected = $(".validateTips");
-    var selection = $("input[name=templating]:checked").val();
+    var selection = $(".focus").text();
 
-    selected.removeClass("ui-state-error");
-
-    if (selection === undefined) {
-        valid = false;
-        selected.addClass("ui-state-error").css("display", "block");
-    } else {
-        $(dialog).dialog("close");
-        self.loadSelectedTemplate(selection, self);
-    }
-
-    return valid;
+    $(dialog).dialog("close");
+    self.loadSelectedTemplate(selection, self);
 };
 
 /**
@@ -104,7 +93,7 @@ XMLTemplates.prototype.loadSelectedTemplate = function(selection, self) {
     if(self.editor === undefined) { self.editor = self; }
 
     $.ajax({
-        url: this.template_path + selection,
+        url: this.template_path + this._formatFormSubmitText(selection),
         dataType: "xml"
     }).done(function(data) {
         var xml_string = self.editor.xml2Str(data);
@@ -114,4 +103,64 @@ XMLTemplates.prototype.loadSelectedTemplate = function(selection, self) {
         self.editor.loadSchema(self.editor.options.schema);
         alert("Unable to load the requested template: " + textStatus);
     });
+};
+
+/**
+ * Highlight and focus currently selected template
+ */
+XMLTemplates.prototype.focusTemplate = function() {
+    $('#dialog-form ul').on('click keydown', function(e) {
+        e.preventDefault();
+        var key = e.which;
+        var number_of_forms, base_element, current, form_id, next_element;
+
+        if(key === 1 || key === 9) {
+            number_of_forms = $('#dialog-form ul li').length;
+
+            if(key == 1) {
+                base_element = $('#' + e.target.id);
+            } else if(key === 9) {
+                current = $('.focus').attr('id');
+                form_id = parseInt(current.slice(-1)) + 1;
+                next_element = (form_id === number_of_forms) ? 0 : form_id;
+                base_element = $('#template_' + next_element);
+            }
+
+            $('.templating').removeClass('focus');
+            base_element.addClass('focus').focus();
+        }
+    });
+
+    $(document)
+};
+
+
+XMLTemplates.prototype.loadFromDirectClick = function(e) {
+    $('#dialog-form').on('dblclick', function(e) {
+
+        this.processForm();
+    });
+};
+
+/**
+ * Format template names for dialog form
+ * Remove file extension
+ * Upper case first letter for template names without extending String prototype
+ * @param string
+ * @returns {string}
+ * @private
+ */
+XMLTemplates.prototype._formatFormText = function(string) {
+    var remove_file_format = string.replace(/\.\w{3,4}$/, '');
+    return remove_file_format.charAt(0).toUpperCase() + remove_file_format.slice(1);
+};
+
+/**
+ * Undo pretty printing to get actual file name to load
+ * @param string
+ * @returns {string}
+ * @private
+ */
+XMLTemplates.prototype._formatFormSubmitText = function(string) {
+    return $.trim(string.toLowerCase()) + '.xml';
 };
