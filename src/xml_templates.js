c5ce0d65e36a939dd70edae537e2dc18472a7d14
lfarrell <lfarrell@email.unc.edu>
2021-03-23 13:26:23 -0400

    Move cancel action to templating options

--- src/xml_templates.js
+++ src/xml_templates.js
@@ -26,14 +26,7 @@ XMLTemplates.prototype.createDialog = function() {
 	var self = this;
 	var buttons = {};
 	if (self.editor.options.templateOptions.cancelFunction) {
-		var cancelTemplate = $.proxy(self.editor.options.templateOptions.cancelFunction, self);
-		if (self.editor.xmlState !== null) {
-			cancelTemplate = function() {
-				self.form.dialog("close");
-				return false
-			}
-		}
-		buttons["Cancel"] = cancelTemplate;
+		buttons["Cancel"] = $.proxy(self.editor.options.templateOptions.cancelFunction, self);
 	}
 	buttons["Choose"] = function() {
 		self.processForm();
